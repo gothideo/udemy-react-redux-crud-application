8c7e6ed553bf145b779fc5a75698912111652716
got.hideo <got.hideo@gmail.com>
2021-11-21 18:27:19 +0900

    parctice jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 function App() {
   return (
-    <div><h1>Hello, World!</h1></div>
+    <h1>Hello, World!</h1>
   );
 }
+// import React,{ Component} from 'react';
+// class App extends Component {
+//   render(){
+//     return React.createElement(
+//       "h1",
+//       null,
+//       "Hello,World!!"
+//     );
+//   }
+// }
 
 export default App;
