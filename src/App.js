d49ba0cea04dba8db0cc209a69488ed248be69c5
got.hideo <got.hideo@gmail.com>
2021-11-21 18:50:21 +0900

    Learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from "react";
+
 function App() {
+  // const dom = <input type="button" onClick={() => {alert("I am clicked.");}}/>;
+  const dom = <React.Fragment><label htmlFor="bar">bar</label><input type="text" onChange={() => {alert("I am changed.");}}/></React.Fragment>;
   return (
-    <h1>Hello, World!</h1>
+    dom
   );
 }
+// function App() {
+//   const greeting = "Hi!!";
+//   const dom = <h1 className="foo">{greeting}</h1>;
+//   return (
+//     dom
+//   );
+// }
 // import React,{ Component} from 'react';
 // class App extends Component {
 //   render(){
