fb6cdb61fd4a7f3251d0fd80e0fcd13aa67103a8
got.hideo <got.hideo@gmail.com>
2022-01-14 08:06:39 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from "react";
-
-function App() {
-  // const dom = <input type="button" onClick={() => {alert("I am clicked.");}}/>;
-  const dom = <React.Fragment><label htmlFor="bar">bar</label><input type="text" onChange={() => {alert("I am changed.");}}/></React.Fragment>;
-  return (
-    dom
-  );
-}
-// function App() {
-//   const greeting = "Hi!!";
-//   const dom = <h1 className="foo">{greeting}</h1>;
-//   return (
-//     dom
-//   );
-// }
+// クラスcomponent
 // import React,{ Component} from 'react';
+
 // class App extends Component {
 //   render(){
-//     return React.createElement(
-//       "h1",
-//       null,
-//       "Hello,World!!"
-//     );
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {alert("I am changed.");}}/>
+//       </React.Fragment>
+//     )
 //   }
 // }
 
+// ファンクションコンポーネント
+import React from 'react';
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
 export default App;
