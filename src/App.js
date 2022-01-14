baa9b71bcd663b74043ee68ccea6ea331fc335e4
got.hideo <got.hideo@gmail.com>
2022-01-14 21:55:53 +0900

    use props

--- src/App.js
+++ src/App.js
@@ -15,16 +15,28 @@
 // ファンクションコンポーネント
 import React from 'react';
 const App = () => {
+  const profiles =[
+    {name: "Taro",age:10},
+    {name: "HanakoOOO",age: 11},
+    {}
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile,index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi I am {props.name} and I am {props.age} years old</div>
 }
+
+User.defaultProps = {
+  name:"MR.x",age:1
+}
+
 export default App;
